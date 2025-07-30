902dda7bb58c1fa2ac1e60693a400f0b1877f5de
michael.koob <michael.koob@klarna.com>
2025-07-30 12:21:07 +0200

    Add frontend handler

--- backend/src/main/java/com/example/cattracker/configuration/WebConfig.java
+++ backend/src/main/java/com/example/cattracker/configuration/WebConfig.java
@@ -15,12 +15,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-
-                registry.addMapping("/api/**")
-                        .allowedOrigins("https://**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                        .allowedOrigins("https://*.fly.dev", "http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
 
             @Override

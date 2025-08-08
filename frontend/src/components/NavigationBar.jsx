73ec90337edb08397310d8cb7b5e357a76308ad0
michael.koob <michael.koob@klarna.com>
2025-08-08 10:56:10 +0200

    Add time of day indicator background

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,5 +1,7 @@
 import { formatDisplayDate, getNextFeeding, getPrevFeeding } from '../utils/date.js';
 import { getTodayString, getFeedingTime} from '../utils/date.js';
+import morningImage from '../assets/morning.jpg';
+import eveningImage from '../assets/evening.jpg';
 
 export default function NavigationBar({ date, timeOfDay, navigate }) {
   const handleNav = (direction) => {
@@ -10,16 +12,34 @@ export default function NavigationBar({ date, timeOfDay, navigate }) {
   };
 
   const displayTime = timeOfDay === 'MORNING' ? 'Morgens' : 'Abends';
+  const isMorning = timeOfDay === 'MORNING';
 
   return (
-    <div className="flex items-center justify-between px-4 py-2 border-b">
-      <button className="text-8xl" onClick={() => handleNav('left')}>&larr;</button>
-      <div className="text-center">
-        <div className="text-8xl font-semibold">{formatDisplayDate(date)}</div>
-        <div className="text-6xl text-gray-600">{displayTime}</div>
-        <button className="text-6xl text-blue-600" onClick={() => navigate(`/${getTodayString()}/${getFeedingTime()}`)}>Heute</button>
+    <div className="relative p-4 flex justify-between items-center">
+
+
+      <div className="absolute inset-0 bg-center bg-cover" style={{
+        backgroundImage: `url(${ isMorning ? morningImage : eveningImage})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+      }}></div>
+      {/* Fade-Overlay an den Rändern */}
+      <div className={`absolute inset-0 pointer-events-none bg-gradient-to-r 
+        ${isMorning ? 'from-white/70' : 'from-white/40'} 
+        via-transparent 
+        ${isMorning ? 'to-white/70' : 'to-white/40'}
+     `}></div>
+
+      <div className={`flex relative z-10 items-center justify-between px-4 py-2 ${
+        isMorning ? 'text-black' : 'text-white'
+      }`}>
+        <button className="text-8xl" onClick={() => handleNav('left')}>&larr;</button>
+        <div className="text-center" onClick={() => navigate(`/${getTodayString()}/${getFeedingTime()}`)}>
+          <div className="text-8xl font-semibold">{formatDisplayDate(date)}</div>
+          <div className="text-6xl text-gray-400">{displayTime}</div>
+        </div>
+        <button className="text-8xl" onClick={() => handleNav('right')}>&rarr;</button>
       </div>
-      <button className="text-8xl" onClick={() => handleNav('right')}>&rarr;</button>
     </div>
   );
 }
